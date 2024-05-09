b1e859266c079f8826d121bfec1ba9bb2992b495
Davide Cappellari <davide@tecno-system.eu>
2024-05-09 16:15:32 +0200

    First commit

---
+++ samples/CodeSculpt.Sample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CodeSculpt.Extensions;
+
+var a = "Marco";
+var b = "marco";
+
+if (a.EqualsIgnoreCase(b))
+{
+    Console.WriteLine("Sono uguali");
+}
+else
+{
+    Console.WriteLine("Sono diverse");
+}
+
+string? personId = null;
+
+var name = personId ?? "(sconosciuto)";
+
+var priority = Priority.Low;
+
+Console.WriteLine($"Priority is {priority.GetDescription()}");
+
+var strings = new List<string>() { "VALORE1", "VALORE2" };
+
+foreach (var item in strings.WithIndex())
+{
+    Console.WriteLine(item.Index + " " + item.Value);
+}
+
+public enum Priority
+{
+    [Display(Name = "Bassa")]
+    Low,
+    Medium,
+    High
+}
+
